17e6911161ecdd62e14637e31a1f03ae7ac9a391
Gurram sahithi priya <59274924+sahithipriya7419@users.noreply.github.com>
2021-04-14 13:22:09 +0530

    Added login.php for backend

---
+++ sahil/backend/login.php
@@ -0,0 +1,29 @@
+<?php
+ 
+if($_SERVER['REQUEST_METHOD'] = 'POST')
+{
+     include 'dbconnect.php';
+
+     $user = $_POST['username'];
+     $password = $_POST['password'];
+
+     $sql = "SELECT * FROM `user` WHERE Username='$user' AND Password = md5('$password')";
+
+     $result = mysqli_query($conn, $sql);
+
+     $num = mysqli_num_rows($result);    
+
+     if($num>0)
+     {
+        $row = mysqli_fetch_assoc($result);
+        echo $row['Username'] . 'password: ' . $row['Password'];
+
+     }
+     else{
+         echo "Invalid user";
+     }
+
+}
+
+
+?>
