d64b2ba514a6f867aef55b4fccc1c5c2fbf44a35
Sahil <soilshubham@gmail.com>
2021-04-22 04:11:17 +0530

    improved cart functionality and added profile icon on navbar
    added delete from cart, bubbles on menu item, total amount

--- sahil/js/cart.js
+++ sahil/js/cart.js
@@ -6,10 +6,32 @@ book_order.addEventListener('click',place_order);
 var totalItems = 10;
 cartArray = [];
 
+function updateTotalAmount(){
+    var totalAmountToPay = 0
+
+    Array.from(cartArray).forEach((element)=>{
+        totalAmountToPay += element.price*element.q;
+    });
+    document.getElementById("total-amount").textContent = "Rs." + totalAmountToPay;
+}
+
+updateTotalAmount();
+
+
 function openCart(val){
-    if(val) cart.style.display = "initial";
-    else cart.style.display = "none";
-    Array.from(cartArray).forEach((element)=>{console.log(element['id'])});
+
+    if(val) {
+        cart.style.display = "initial";
+        document.body.style.overflow = "hidden";
+    }
+    else{
+        cart.style.display = "none";
+        document.body.style.overflow = "visible";
+    }
+    if(cartArray.length>0) document.getElementById("cart-items").childNodes[0].textContent = "";
+    else document.getElementById("cart-items").childNodes[0].textContent = "Your Cart Is Empty";
+    
+    updateTotalAmount();
 }
 
 var foodItems = 
@@ -83,6 +105,28 @@ function cartElement(id, name, price){
     this.q = 1;
 };
 
+
+function changeBubble(a){
+    var bubble = a.parentElement.parentElement.firstElementChild.getElementsByClassName("bubble")[0];
+    bubble.style.display = "initial";
+    var bubbleValue = parseInt(bubble.textContent);
+    if(Number.isInteger(bubbleValue)){
+        bubble.innerHTML = bubbleValue+1;
+    }
+    else{
+        bubble.innerHTML = 1;
+    }
+}
+
+function btnPopFX(a, c){
+    if(c)
+    a.style.transform = "scale(1.1)";
+    else
+    a.style.transform = "scale(1)";
+}
+
+
+
 function searchItem(idKey, myArray){
     for (var i=0; i < myArray.length; i++) {
         if (myArray[i].id === idKey) {
@@ -92,32 +136,124 @@ function searchItem(idKey, myArray){
 };
 
 function addItem(a){
+
+    changeBubble(a);
+    a.style.transform = "scale(0.9)";
+    setTimeout(() => {
+        a.style.transform = "scale(1.1)";
+    }, 100);
+
     var item = searchItem(a.parentElement.parentElement.id, foodItems);
-    var cartItem = new cartElement(item.id, item.name, item.price);
-    cartArray.push(cartItem);
 
-    var parentitem = document.createElement("div");  
-    parentitem.classList.add("item");  
-    var itemName = document.createElement("div");
-    itemName.classList.add("item-name");
-    var itemPrice = document.createElement("div");
-    itemPrice.classList.add("item-price");
-    var itemQuantity = document.createElement("div");
-    itemQuantity.classList.add("item-quantity");
-    var deleteBtn = document.createElement("a");
-    deleteBtn.classList.add("item-delete");
+    var present = false;
+    if(cartArray.length !=0){
+        for(var i=0; i<cartArray.length; i++){
+            if(item.id != cartArray[i].id){
+                present = false;
+            }
+            else{
+                present = true;
+                break;
+            }
+        }
+    }
+    
+    if(present){
+        cartArray[i].q +=1;
+        var itemPresent_quantity = document.getElementById("cart-items").childNodes;
+        for(var i=0; i<itemPresent_quantity.length; i++){
+            if("cart-"+item.id == itemPresent_quantity[i].id){
+                var presetElement = itemPresent_quantity[i].getElementsByClassName("item-quantity")[0];
+                console.log("hehe", presetElement);
+                presetElement_q = parseInt(presetElement.textContent);
+                presetElement.innerHTML = presetElement_q+1;
 
-    itemName.appendChild(document.createTextNode(cartItem.name));
-    itemPrice.appendChild(document.createTextNode(cartItem.price));
-    itemQuantity.appendChild(document.createTextNode(cartItem.q));
-    deleteBtn.appendChild(document.createTextNode("Delete"));
-    parentitem.appendChild(itemName);
-    parentitem.appendChild(itemPrice);
-    parentitem.appendChild(itemQuantity);
-    parentitem.appendChild(deleteBtn);
+            }
+        }
+    }
+    else{
+        var cartItem = new cartElement(item.id, item.name, item.price);
+        cartArray.push(cartItem);
+    
+        var parentitem = document.createElement("div"); 
+        parentitem.id = "cart-"+cartItem.id; 
+        parentitem.classList.add("item");  
+        var itemName = document.createElement("div");
+        itemName.classList.add("item-name");
+        var itemPrice = document.createElement("div");
+        itemPrice.classList.add("item-price");
+        var itemQuantity = document.createElement("div");
+        itemQuantity.classList.add("item-quantity");
+        var deleteBtn = document.createElement("a");
+        deleteBtn.addEventListener("click", function(){
+            removeItem(this);
+        });
+        deleteBtn.classList.add("item-delete");
+    
+        itemName.appendChild(document.createTextNode(cartItem.name));
+        itemPrice.appendChild(document.createTextNode(cartItem.price + "/-"));
+        itemQuantity.appendChild(document.createTextNode(cartItem.q));
+        deleteBtn.appendChild(document.createTextNode("Delete"));
+        parentitem.appendChild(itemQuantity);
+        parentitem.appendChild(itemName);
+        parentitem.appendChild(itemPrice);
+        parentitem.appendChild(deleteBtn);
+    
+        document.getElementById("cart-items").appendChild(parentitem);
+    }
 
-    document.getElementById("cart-items").appendChild(parentitem);
+    
     console.log(cartArray);
+}
+
+function removeItem(a){
+    
+
+    var itemID = a.parentElement.id;
+
+    var mainDish =  document.getElementById("mainDish-items").childNodes;
+    var dessert =  document.getElementById("dessert-items").childNodes;
+
+    var menuPresent = false;
+
+    for(var i=0; i<mainDish.length; i++){
+        if(itemID == "cart-"+mainDish[i].id){
+            // console.log("ggg", mainDish[i].getElementsByClassName("image-container")[0].getElementsByClassName("bubble")[0].textContent);
+            mainDish[i].getElementsByClassName("image-container")[0].getElementsByClassName("bubble")[0].textContent = "0";
+            mainDish[i].getElementsByClassName("image-container")[0].getElementsByClassName("bubble")[0].style.display = "none";
+        }
+    }
+    for(var i=0; i<dessert.length; i++){
+        if(itemID == "cart-"+dessert[i].id){
+            // console.log("ggg", mainDish[i].getElementsByClassName("image-container")[0].getElementsByClassName("bubble")[0].textContent);
+            dessert[i].getElementsByClassName("image-container")[0].getElementsByClassName("bubble")[0].textContent = "0";
+            dessert[i].getElementsByClassName("image-container")[0].getElementsByClassName("bubble")[0].style.display = "none";
+        }
+    }
+
+
+    var elemToBeDeleted = a.parentElement;
+    elemToBeDeleted.remove();
+
+    var cartPresent = false;
+    if(cartArray.length !=0){
+        for(var i=0; i<cartArray.length; i++){
+            if(itemID != "cart-"+cartArray[i].id){
+                cartPresent = false;
+            }
+            else{
+                cartPresent = true;
+                break;
+            }
+        }
+        if(cartPresent) {
+            cartArray.splice(i, 1);
+            updateTotalAmount();
+        }
+    }
+
+    if(cartArray.length>0) document.getElementById("cart-items").childNodes[0].textContent = "";
+    else document.getElementById("cart-items").childNodes[0].textContent = "Your Cart Is Empty";
 }
 
 function place_order(){
